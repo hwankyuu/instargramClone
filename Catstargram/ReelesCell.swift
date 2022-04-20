fc229f1f67165128d93db5d0da2526b4fdf40845
kyuu <ghksrb1226@gmail.com>
2022-04-20 23:10:58 +0900

    릴스아이콘 추가

---
+++ Catstargram/ReelesCell.swift
@@ -0,0 +1,8 @@
+//
+//  ReelesCell.swift
+//  Catstargram
+//
+//  Created by 이환규 on 2022/04/19.
+//
+
+import Foundation
---
+++ Catstargram/VideoPlayerView.swift
@@ -0,0 +1,8 @@
+//
+//  VideoPlayerView.swift
+//  Catstargram
+//
+//  Created by 이환규 on 2022/04/20.
+//
+
+import Foundation
